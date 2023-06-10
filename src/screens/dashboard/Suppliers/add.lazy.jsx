9feb4904307b71757091cd37f98f70c9e5c3ccbd
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2023-06-10 13:42:07 +0500

    Suppliers  pages done

--- src/screens/dashboard/Suppliers/add.lazy.jsx
+++ src/screens/dashboard/Suppliers/add.lazy.jsx
@@ -10,72 +10,28 @@ export default function SupplierAdd() {
       <div className="container__main__content__details__main container__main__content__details__main__special">
         <div className="container__main__content__details__main__row">
           <div className="container__main__content__details__main__row__entry">
-            <Input type="text" label="Company" placeholder="Enter Company" />
-          </div>
-          <div className="container__main__content__details__main__row__entry">
-            <Input
-              type="text"
-              label="Brand Name"
-              placeholder="Enter Brand Name"
-            />
-          </div>
-        </div>
-        <div className="container__main__content__details__main__row">
-          <div className="container__main__content__details__main__row__entry">
-            <Input
-              type="text"
-              label="Vat Number"
-              placeholder="Enter Vat Number"
-            />
-          </div>
-          <div className="container__main__content__details__main__row__entry">
-            <Input
-              type="text"
-              label="Email address"
-              placeholder="Enter email address"
-            />
-          </div>
-        </div>
-        <div className="container__main__content__details__main__row">
-          <div className="container__main__content__details__main__row__entry">
-            <Input type="text" label="Phone No." placeholder="Enter Phone" />
+            <Input type="text" label="Supplier Name" placeholder="Enter Name" />
           </div>
           <div className="container__main__content__details__main__row__entry">
             <Select
-              label="State"
-              placeholder="Select State"
+              label="Status"
+              placeholder="Active"
               options={[
-                { value: "punjab", label: "punjab" },
-                { value: "khaber", label: "khaber" },
-                { value: "bhr", label: "bhr" },
+                { value: "Active", label: "Active" },
+                { value: "Inactive", label: "Inactive" },
               ]}
             />
           </div>
         </div>
         <div className="container__main__content__details__main__row">
           <div className="container__main__content__details__main__row__entry">
-            <Select
-              label="City"
-              placeholder="Select City"
-              options={[
-                { value: "Lahore", label: "Lahore" },
-                { value: "Ali pur", label: "Ali pur" },
-                { value: "Fsd", label: "Fsd" },
-              ]}
-            />
-          </div>
-          <div className="container__main__content__details__main__row__entry">
             <Input
-              type="text"
-              label="Postal code"
-              placeholder="Enter Postal code"
+              type="email"
+              label="Supplier Email"
+              placeholder="Enter Email "
             />
           </div>
         </div>
-
-        <Input type="text" label="Address" placeholder="Enter Address" />
-
-        <Textarea label="Description" placeholder="Enter Description" />
       </div>
       <div className="container__main__content__details__buttons">
         <Link
--- src/screens/dashboard/Suppliers/index.lazy.jsx
+++ src/screens/dashboard/Suppliers/index.lazy.jsx
@@ -2,11 +2,11 @@ import {
   TableEntryDeleteButton,
   TableEntryDescription,
   TableEntryEditButton,
-  TableEntryStatus,
+  StatusButton,
   TableEntryText,
   TableEntryViewButton,
+  FilterButton,
 } from "components";
-
 import { Link } from "router";
 import { Search } from "react-feather";
 import { useLocation } from "react-router-dom";
@@ -33,58 +33,35 @@ export default function Suppliers() {
             to={location.pathname.toLowerCase() + "/add"}
             className="container__main__content__listing__header__right__button"
           >
-            Add
+            Add New
           </Link>
         </div>
       </div>
       <div className="container__main__content__listing__table">
         <div className="container__main__content__listing__table__header">
           <div className="container__main__content__listing__table__header__entry">
-            Action
+            Actions
+          </div>
+          <div className="container__main__content__listing__table__header__entry">
+            Supplier ID
+          </div>
+          <div className="container__main__content__listing__table__header__entry">
+            Supplier Name
+          </div>
+          <div className="container__main__content__listing__table__header__entry">
+            Supplier Email
+          </div>
+          <div className="container__main__content__listing__table__header__entry">
+            No. of Orders
           </div>
           <div className="container__main__content__listing__table__header__entry">
             Status
           </div>
           <div className="container__main__content__listing__table__header__entry">
-            Company
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Brand name
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Tax ID
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Email
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Phone
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Address
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            City
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            State
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Postal code
-          </div>
-          <div className="container__main__content__listing__table__header__entry">
-            Description
+            Last Order Date
           </div>
         </div>
         <div className="container__main__content__listing__table__content">
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
-          <TableEntry />
           <TableEntry />
           <TableEntry />
           <TableEntry />
@@ -110,29 +87,17 @@ function TableEntry() {
   return (
     <div className="container__main__content__listing__table__content__list">
       <div className="container__main__content__listing__table__content__list__entry">
-        <TableEntryViewButton to="/john-doe" />
         <TableEntryEditButton />
         <TableEntryDeleteButton />
       </div>
-      <TableEntryStatus />
-      <TableEntryText>comeers</TableEntryText>
-      <TableEntryText>Hello Brand</TableEntryText>
       <TableEntryText>12344</TableEntryText>
-      <TableEntryText>asmcompan@gmail.com</TableEntryText>
-      <TableEntryText>+92-20039394</TableEntryText>
-      <TableEntryDescription>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor,
-        nisl eget ultricies lacinia, nisl nisl aliquam nisl, eu aliquam nisl
-        nisl sit amet nisl.
-      </TableEntryDescription>
-      <TableEntryText>Lhr</TableEntryText>
-      <TableEntryText>khaber</TableEntryText>
-      <TableEntryText>30999</TableEntryText>
-      <TableEntryDescription>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor,
-        nisl eget ultricies lacinia, nisl nisl aliquam nisl, eu aliquam nisl
-        nisl sit amet nisl.
-      </TableEntryDescription>
+      <TableEntryText>Tomorrow Leave</TableEntryText>
+      <TableEntryText>abc123@gmail.com</TableEntryText>
+      <TableEntryText>05</TableEntryText>
+
+      <StatusButton text="Active" />
+
+      <TableEntryText>23 May 2023</TableEntryText>
     </div>
   );
 }
